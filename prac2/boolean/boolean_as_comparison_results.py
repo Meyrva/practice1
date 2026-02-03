99a5ad77850f983b741c5733c402550730f9c930
Meyrva <yaraslavka1@gmail.com>
2026-02-03 19:26:54 +0500

    Added examples of boolean in Python (Boolean Values, Boolean as Comparison Results, Boolean Operators)

---
+++ prac2/boolean/boolean_as_comparison_results.py
@@ -0,0 +1,29 @@
+#1
+x = 5
+y = 3
+
+print(x == y)
+#2
+x = 534
+y = 2
+
+print(x != y)
+
+#3
+x = 0
+y = 7
+
+print(x > y)
+
+#4
+x = 12
+y = 59
+
+print(x >= y)
+
+#5
+x = 2
+
+print(1 < x < 10)
+
+print(1 < x and x < 10)
---
+++ prac2/boolean/boolean_operators.py
@@ -0,0 +1,28 @@
+#1
+x = 5
+
+print(1 < x < 10)
+
+print(1 < x and x < 10)
+#2
+x = ["apple", "banana"]
+y = ["apple", "banana"]
+z = x
+
+print(x is z)
+print(x is y)
+print(x == y)
+#3
+x = 5
+
+print(x < 5 or x > 10)
+#4
+x = 5
+
+print(not(x > 3 and x < 10))
+#5
+x = [1, 2, 3]
+y = [1, 2, 3]
+
+print(x == y)
+print(x is y)
