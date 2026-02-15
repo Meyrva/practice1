b6d484021c112eb626ee2ca385f525ce1f746274
Meyrva <yaraslavka1@gmail.com>
2026-02-15 22:48:22 +0500

    Python inheritance

---
+++ prac3/inheritance/inheritance_basics.py
@@ -0,0 +1,31 @@
+#1  Any class can be a parent class, so the syntax is the same as creating any other class:
+class Person:
+  def __init__(self, fname, lname):
+    self.firstname = fname
+    self.lastname = lname
+
+  def printname(self):
+    print(self.firstname, self.lastname)
+
+x = Person("John", "Doe")
+x.printname()
+#2
+class Person:
+  def __init__(self, fname, lname):
+    self.firstname = fname
+    self.lastname = lname
+
+  def printname(self):
+    print(self.firstname, self.lastname)
+
+class Student(Person):
+  pass
+
+x = Student("Mike", "Olsen")
+x.printname()
+
+#3
+
+#4
+
+#5
